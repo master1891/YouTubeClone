3bbe68f769125059ba804995dfdb62e1fb8a9c6c
Nelson Ek <neltip.ne@gmail.com>
2023-02-24 20:30:59 -0600

    Commit clase 2

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Nelson Gustavo Ek Perera on 24/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
